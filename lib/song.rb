deab6d189fc8785ade5051468ee6310363784c93
staceyko <ko.stacey@gmail.com>
2015-08-05 19:23:54 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,32 @@
 class Song
-  # code goes here
+
+attr_accessor :title, :artist, :serialize
+
+#def title
+#    @title
+#end
+#def title=(title)
+#    @title = title
+#
+#end
+#
+#def artist=(artist)
+#    @artist = artist
+#end
+#
+#def artist
+#    @artist
+#end
+
+
+
+
+def serialize
+    file_title = @title.downcase.tr!(" ", "_")
+    file = Tempfile.new(["#{file_title}", ".txt"], "tmp")
+    file.write("#{@artist.name} - #{@title}")
+    file.rewind
+    file.read
+    file.close
+end
 end
